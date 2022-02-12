8b55a57270e0b92e2e81d8070aa188bb28c15fdf
fire833 <kttpsy@gmail.com>
2022-02-12 16:40:40 -0600

    Worked on getting template data interfaces for Aaron.

---
+++ spearphishing/src/containers/news_emails/NewsEmailTemplates.tsx
@@ -0,0 +1,14 @@
+
+
+export class NewsEmailTemplate {
+
+}
+
+export enum NewsType {
+    LocalPositive,
+    GlobalPositive,
+    LocalNegative,
+    GlobalNegative,
+    LocalHorrible,
+    GlobalHorrible
+}
---
+++ spearphishing/src/containers/users/users.ts
@@ -0,0 +1,17 @@
+import {User, UserOccupation} from './UserContainer'
+
+export const GlobalUsers: User[] = [{
+    Gender: "male",
+    Picture: "", // TBD
+    Age: 25,
+    Name: "Kevin Wolfram",
+    Occupation: UserOccupation.Accountant,
+},
+{
+    Gender: "female",
+    Picture: "", // TBD
+    Age: 37,
+    Name: "Miranda Gates",
+    Occupation: UserOccupation.ArmySergeant,
+}];
+
