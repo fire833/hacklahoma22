e9d1b71d0dee0f5cecb25752546ed8126bd33527
fire833 <kttpsy@gmail.com>
2022-02-13 00:50:26 -0600

    Worked on templating attack vector emails

--- spearphishing/src/features/InboxEmails/DraftEmail/EmailAttackStrategies/BankResetPasswordEmail.tsx
+++ spearphishing/src/features/InboxEmails/DraftEmail/EmailAttackStrategies/BankResetPasswordEmail.tsx
@@ -1,6 +1,22 @@
+import { TargetList } from "../../TargetBriefEmail/TargetList/TargetList";
+import { addName, addLink } from "./GlobalDecisionMethods";
 
+// This needs some work but its a good start.
 export function BankResetPasswordEmail() {
     return (
-        <p></p>
+        <p> 
+            Hello {addName("user", "")}, 
+            Your bank account has been disabled due to an
+            extreme number of incorrect logins originating
+            from a hostile source. For your security, please
+            go to the below link in order to reset your bank 
+            password.
+
+            {addLink("https://mybank.co", "https://baankofamerica.com")}
+
+            Thank you very much,
+            
+            Your banking team
+        </p>
     )
 }
---
+++ spearphishing/src/features/InboxEmails/DraftEmail/EmailAttackStrategies/GlobalDecisionMethods.ts
@@ -0,0 +1,24 @@
+// import { useAppSelector } from "../../../../app/hooks";
+
+// Adds a name for a given template, returns default if the
+// user has not enabled real personal information on this 
+// phishing email.
+export function addName(def: string, fancy: string): string {
+    if (true) {
+        return fancy;
+    } else {
+        return def;
+    }
+}
+
+// Use either a fancy/better link address, or the worse one depending on
+// domain strength.
+//
+// TODO needs to have multiple values based on domain level rather than just fancy & default.
+export function addLink(def: string, fancy: string): string {
+    if (true) {
+        return fancy;
+    } else {
+        return def;
+    }
+}
