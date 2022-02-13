d8049cfea42a81a0b7eb6f6462f1a2ea372a04d9
fire833 <kttpsy@gmail.com>
2022-02-13 08:21:22 -0600

    Got initial scame templates done

--- spearphishing/src/features/InboxEmails/DraftEmail/EmailAttackStrategies/RelativeInPrisonEmail.tsx
+++ spearphishing/src/features/InboxEmails/DraftEmail/EmailAttackStrategies/RelativeInPrisonEmail.tsx
@@ -1,6 +1,28 @@
 
 export function RelativeInPrisonEmail() {
     return (
-        <p></p>
+        <div className="emailContent">
+            <p>
+                Good morning relative, 
+            </p>
+            <p>
+                It is great to hear from you! I don't know if you heard,
+                but your uncle Jimmiy got in a bit of a hairy drunken fight
+                with a cop the other night, and he is in the local jail in 
+                pretty bad shape, and I don't have the money to bail him out. 
+            </p>
+            <p>
+                Could you do me a favor with this? He is in very poor shape, and
+                needs to get out. If you could just wire $500 over to him in order
+                to get him out, him and I would definitely appreciate it, if you
+                can't send all of it, that's fine, he just really needs to get out.
+            </p>
+            <p>
+                Much love,
+            </p>
+            <p>
+                Auntie
+            </p>
+        </div>
     )
 }
