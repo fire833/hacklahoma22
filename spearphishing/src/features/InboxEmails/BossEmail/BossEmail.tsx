6da30b6c5df4622b32a810c56929317429f8a4e5
fire833 <kttpsy@gmail.com>
2022-02-12 19:21:47 -0600

    updated to BossEmail bossStatusemail

--- spearphishing/src/features/InboxEmails/BossEmail/BossEmail.tsx
+++ spearphishing/src/features/InboxEmails/BossEmail/BossEmail.tsx
@@ -2,7 +2,7 @@ import { InboxEmailKind } from "../../inbox/ActiveEmail";
 import { InboxEmailProps } from "../InboxEmailProps";
 
 
-export function BossEmail(props: InboxEmailProps){
+export function BossStatusEmail(props: InboxEmailProps){
 
     if(props.activeEmail === null || props.activeEmail.kind !== InboxEmailKind.BossEmail) return <></>
 
